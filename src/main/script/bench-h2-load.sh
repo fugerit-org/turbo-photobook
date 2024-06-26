8f634624ff3e75c4afae17467cdf067ebf0c00ff
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-06-26 18:15:37 +0200

    Benchmark

--- src/main/script/bench-h2-load.sh
+++ src/main/script/bench-h2-load.sh
@@ -5,6 +5,10 @@
 # Author:	Matteo Franci <mttfranci@gmail.com>
 set -e
 
+NUMBER_OF_REQUESTS=1000000
+NUMBER_OF_CLIENTS=30
+NUMBER_OF_THREADS=2
+
 function print() {
     printf "\033[1;34m$1\033[0m\n"
 }
@@ -13,6 +17,10 @@ declare -a URL_LIST
 URL_LIST[0]='list?v=1718975879495'
 URL_LIST[1]='download/springio23_1000.jpg'
 URL_LIST[2]='images/springio23/language/it/current_page/1/page_size/5?v=1718975898349'
+URL_LIST[3]='download/turbo-photobook_2000.jpg'
+URL_LIST[4]='images/turbo-photobook/language/en/current_page/1/page_size/5?v=1718975898349'
+URL_LIST[5]='download/turbo-photobook_2001.jpg'
+URL_LIST[6]='download/turbo-photobook_2002.jpg'
 
 BASE_URL=${1}
 
@@ -25,6 +33,6 @@ else
     export URL_PARAM="${URL_PARAM} ${REST_BASE}/${url}"
   done
   echo "URL_PARAM : ${URL_PARAM}"
-  h2load -n100000 -c50 --warm-up-time=2 ${URL_PARAM}
+  h2load -n${NUMBER_OF_REQUESTS} -c${NUMBER_OF_CLIENTS} -t${NUMBER_OF_THREADS} --warm-up-time=2 ${URL_PARAM}
   print "Benchmark over for '${BASE_URL}' 🏎️"
 fi
